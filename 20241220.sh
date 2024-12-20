ffff608a5224ff0beb3cbebc570172b634654805
zphgzpkf <zphgzpkfx38354@gmail.com>
2024-12-20 20:48:51 +0800

    Update 20241220.sh

--- 20241220.sh
+++ 20241220.sh
@@ -1,8 +1,3 @@
-# https://m.do.co/c/64579858f8d8
-
-
-# wmc0827XX
-
 apt update
 apt install unzip -y
 
@@ -17,13 +12,8 @@ source ~/.bashrc
 # download and install Node.js
 fnm use --install-if-missing 22
 
-# verifies the right Node.js version is in the environment
-node -v # should print `v22.12.0`
 
-# verifies the right npm version is in the environment
-npm -v # should print `10.9.0`
 
-------------------------------------------------
 npm install -g npm@11.0.0
 
 npm i -g rivalz-node-cli
@@ -35,7 +25,3 @@ apt install screen
 screen -S znoderivalz
 
 rivalz run
-
- # 0x748964d0028e407a539d257c8490cb9a5664f330
-
-
