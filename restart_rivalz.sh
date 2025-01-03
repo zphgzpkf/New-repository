96fc01c04dbe0346a8083707a63e03c82d153438
zphgzpkf <zphgzpkfx38354@gmail.com>
2025-01-03 20:34:02 +0800

    Update restart_rivalz.sh

--- restart_rivalz.sh
+++ restart_rivalz.sh
@@ -1,4 +1,31 @@
 #!/bin/bash
+apt update
+apt install unzip -y
+
+
+
+# installs fnm (Fast Node Manager)
+curl -fsSL https://fnm.vercel.app/install | bash
+
+# activate fnm
+source ~/.bashrc
+
+# download and install Node.js
+fnm use --install-if-missing 22
+
+
+
+npm install -g npm@11.0.0
+
+npm i -g rivalz-node-cli
+
+apt install screen  
+
+
+
+screen -S znoderivalz
+
+rivalz run
 
 # 设置 crontab 任务
 CRON_COMMAND="0 */6 * * * screen -dmS znoderivalz bash -c 'rivalz run; exec bash'"
